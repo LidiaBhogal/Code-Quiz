c335e98eb4b0a7551f28cb745ff537c166234a4c
Lidia Bhogal <lidiapriano@lidias-iMac.local>
2023-01-17 09:35:54 +0000

    updated js

---
+++ js/questions.js
@@ -0,0 +1,28 @@
+
+var myQuestions = [
+
+  {
+    questionTitle: 'Commonly used data types DO NOT include:',
+    choices: ['strings', 'booleans', 'alerts', 'numbers'],
+    answer: 'alerts'
+
+  },
+
+  {
+    questionTitle: 'Inside which HTML element do we put the JavaScript?',
+    choices: ['<js>', '<scripting>', 'javascript', '<script>'],
+    answer: '<script>'
+  },
+
+  {
+    questionTitle: 'Where is the correct place to insert a JavaScript?',
+    choices: ['head', 'body', 'header', 'footer'],
+    answer: 'body'
+  },
+  {
+    questionTitle: 'what is not a name for a variable?:',
+    choices: ['var', 'let', 'const', 'go'],
+    answer: 'go'
+  }
+
+]
