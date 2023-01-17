4e31e5a46283469846f840a9c6ee5bca4f032e56
Lidia Bhogal <lidiapriano@lidias-iMac.local>
2023-01-17 10:47:22 +0000

    updated js

--- js/logic.js
+++ js/logic.js
@@ -2,14 +2,11 @@ var startButton = document.getElementById("start");
 var questionsDiv = document.getElementById('questions')
 var questionTitle = document.getElementById('question-title')
 var choices = document.getElementById('choices')
-// var answer =
-var time = document.getElementById('time')
-var timer = document.getElementById('timer')
-var isWin = false;
 
 var questionIndex = 0
 
-
+var time = document.getElementById('time');
+var count = 60
 startButton.addEventListener("click", startGame)
   
 
@@ -26,7 +23,7 @@ function setNextQuestion (){
   if (myQuestions[questionIndex]) {
     showQuestion()
   } else {
-    console.log('game over!')
+    alert('game over!')
   }
 
 }
@@ -59,57 +56,9 @@ function showQuestion(question){
 }
 
 
-function selectAnswer (){
-
-}
 
 
 
-function startTimer() {
-  // Sets timer
-  time = setInterval(function() {
-    time--;
-    timer.textContent = time;
-    if (time >= 0) {
-      // Tests if win condition is met
-      if (isWin && timer > 0) {
-        // Clears interval and stops timer
-        clearInterval(time);
-        winGame();
-      }
-    }
-    // Tests if time has run out
-    if (timer === 0) {
-      // Clears interval
-      clearInterval(time);
-      loseGame();
-    }
-  }, 1000);
-}
-
-
-
-// var choices = document.querySelectorAll('choices');
-
-// for (var i = 0; i < choices.length; i++) {
-//    choices[i].setAttribute('style', color: red) 
-   
-// }
-// function showQuestion(myQuestions){
-//    var questions = document.getElementsByName('questions');
-
-//    questions.textContent = myQuestions.title;
-
-//    var choices= document.querySelectorAll('.choice');
-//    console.log(choices);
-   
-//    choices.forEach(function(element,index){
-//       element.textContent = myQuestions.choices;
-//    }
-//    )
-
-// }
-// showQuestion(myQuestions);
 
 
 
